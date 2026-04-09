c3648a45ee641c36891e0afcf0f4bf9cb0b04fbb
felkng <felipersf374@gmail.com>
2026-04-09 16:33:54 -0300

    chore(config): update hyprland and spotify-toggle configurations

---
+++ bin/spotify-toggle.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get Spotify window info (take the first one if multiple)
+spotify_info=$(hyprctl clients -j | jq -r '.[] | select(.class == "Spotify") | limit(1; .)')
+
+if [ -z "$spotify_info" ] || [ "$spotify_info" == "null" ]; then
+    # Spotify is not running, start it
+    spotify &
+    # Also start the tray helper if it's not running
+    if ! pgrep -x "spotify-tray-way" > /dev/null; then
+        spotify-tray-wayland &
+    fi
+    exit 0
+fi
+
+# Get current workspace and Spotify window address/workspace
+current_workspace=$(hyprctl activeworkspace -j | jq -r .id)
+spotify_addr=$(echo "$spotify_info" | jq -r .address)
+spotify_workspace=$(echo "$spotify_info" | jq -r .workspace.id)
+spotify_workspace_name=$(echo "$spotify_info" | jq -r .workspace.name)
+
+# If it's in a special workspace (id < 0 or name starts with special:), move to current
+if [[ "$spotify_workspace" -lt 0 ]] || [[ "$spotify_workspace_name" == special:* ]]; then
+    hyprctl dispatch movetoworkspace "$current_workspace,address:$spotify_addr"
+    hyprctl dispatch focuswindow "address:$spotify_addr"
+else
+    # If it's in a normal workspace, move to special (minimize)
+    hyprctl dispatch movetoworkspacesilent special:spotify,address:$spotify_addr
+    notify-send "Spotify" "Minimizado para segundo plano" -i spotify
+fi
--- hypr/UserScripts/smart-close.sh
+++ hypr/UserScripts/smart-close.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Smart close: minimize Spotify to tray, kill other windows
-class=$(hyprctl activewindow | grep "class:" | cut -d' ' -f2)
+# Smart close: toggle Spotify (minimize to special), kill other windows
+class=$(hyprctl activewindow -j | jq -r .class)
 if [[ "${class,,}" == "spotify" ]]; then
-    hyprctl dispatch movetoworkspacesilent special:spotify
+    /home/felkng/.local/bin/spotify-toggle.sh
 else
     hyprctl dispatch killactive
 fi
